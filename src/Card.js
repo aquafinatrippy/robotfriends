c5b57d5cf84d7ac8f238b0e1a83dbf1494db2969
Tanel Tekko <45185224+aquafinatrippy@users.noreply.github.com>
2019-05-09 18:00:20 +0300

    Näitab roboteid ja all olevat infot nende kohta

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+    return(
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}`} alt="Robot" width='200' height='200'/>
+            <div className='tc'>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
