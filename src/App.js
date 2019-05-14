e2e4812ec7c5a7b3619f8117502cb5538988656e
Tanel Tekko <45185224+aquafinatrippy@users.noreply.github.com>
2019-05-14 23:06:10 +0300

    Loplik versioon failihaldus veel teha

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Cardlist from './Cardlist';
-import { robots } from './robots';
 import Searchbox from './Searchbox';
 import './app.css';
+import Scroll from './Scroll';
 
 
 export default class App extends React.Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield : ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json();
+        }).then(users =>{
+            this.setState({robots: users});
+        })
+
+        
     }
 
     onSearchChange = (event) => {
@@ -23,13 +33,18 @@ export default class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if(this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        }else{
         return(
         <div className='tc'>
-        <h1 className='f1'>RobotFriends</h1>
-        < Searchbox searchChange={this.onSearchChange}/>
-        <Cardlist robots={filteredRobots}/>
+            <h1 className='f1'>RobotFriends</h1>
+            < Searchbox searchChange={this.onSearchChange}/>
+            <Scroll>
+                <Cardlist robots={filteredRobots}/>
+            </Scroll>
         </div>
-    );
+    );}
     }
     
 }
