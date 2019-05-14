e2a40c51a0206f8c8fdec185c75fdd4f4bcd2f03
Tanel Tekko <45185224+aquafinatrippy@users.noreply.github.com>
2019-05-14 21:09:49 +0300

    Otsing juurde lisatud ja toimib

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import Cardlist from './Cardlist';
 import { robots } from './robots';
 import Searchbox from './Searchbox';
 
-const App = () => {
-    return(
+
+
+export default class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield : ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robots => {
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return(
         <div className='tc'>
         <h1>RobotFriends</h1>
-        < Searchbox />
-        <Cardlist robots={robots}/>
+        < Searchbox searchChange={this.onSearchChange}/>
+        <Cardlist robots={filteredRobots}/>
         </div>
     );
+    }
+    
 }
-export default App;
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Searchbox = () =>{
+const Searchbox = ({searchfield, searchChange}) =>{
     
      return( 
          <div className='pa2'>
-             <input type='search' placeholder='Search robots' className='pa3 ba b--green bg-lightnest-blue'/>
+             <input type='search' placeholder='Search robots' className='pa3 ba b--green bg-lightnest-blue' onChange={searchChange}/>
          </div>
      
      );
